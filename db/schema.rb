d33f5a8ed18fedf4304576febf114b64dd4b5b3d
dannypoit <danny@dannypoit.com>
2020-12-01 14:06:00 -0500

    Add time zone ID to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201201185939) do
+ActiveRecord::Schema.define(version: 20201201190439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20201201185939) do
     t.datetime "updated_at",                                                   null: false
     t.decimal  "current_balance",        precision: 10, scale: 2
     t.integer  "months_to_display",                               default: 3
+    t.integer  "time_zone_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
